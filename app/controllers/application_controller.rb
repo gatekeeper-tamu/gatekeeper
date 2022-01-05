b2625335a28ddf2b842297c032f1e37d75e37657
Rebecca McFadden <rmcfadden@tamu.edu>
2022-01-05 12:13:29 +0200

    bugfix for routing deployed version (#4)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+	Warden::Manager.after_authentication do |user,auth,opts|
+		if !user.username and user.email
+			user.username = user.email.split("@").first
+		end
+	end
+
 	def login
 		authenticate_user!
 	end
