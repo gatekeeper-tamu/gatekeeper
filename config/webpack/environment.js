9c5b0d96dff1062286bcad4e376283cd44796b0d
Rebecca McFadden <rebecca.mcfadden16@gmail.com>
2022-02-03 16:57:57 -0600

    show/hide password

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,11 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require("webpack")
 
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
-}))
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
 
 module.exports = environment
