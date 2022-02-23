bb2005ce36d252e94d2504a5866f8cb26c6129ff
gracenli <gracenecity@gmail.com>
2022-02-23 10:27:07 -0600

    added Reminder UUID

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_043730) do
+ActiveRecord::Schema.define(version: 2022_02_23_161818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2022_02_23_043730) do
     t.index ["user_id"], name: "index_memberships_on_user_id"
   end
 
-  create_table "reminders", force: :cascade do |t|
+  create_table "reminders", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.boolean "recurring"
     t.string "message"
     t.datetime "created_at", precision: 6, null: false
