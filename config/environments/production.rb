7e317f554e390efc0855c6cd193b91f7142fac5d
Grace Li <gracenecity@gmail.com>
2022-02-23 19:58:34 -0600

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -132,8 +132,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => ENV[REMINDERS_EMAIL],
-    :password             => ENV[REMINDERS_PASS],
+    :user_name            => ENV["GMAIL_EMAIL"],
+    :password             => ENV["GMAIL_PASSWORD"],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
