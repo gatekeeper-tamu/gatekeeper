2e9418f984491fb1bdb55ed0cdb14b0461627999
Nikhitha Vempati <nikhithareddyv@gmail.com>
2022-03-09 18:10:21 -0600

    UI set up, search results not showing up yet, need to bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
 	get '/search', to: 'search#index'
 	get 'home/index'
 	root 'home#index'
-
 	resources :reminder, path: '/reminders'
 	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
