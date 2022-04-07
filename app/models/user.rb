d1be4945c53e05dbc842a1a73b8df1274e3faa02
Cora <coraenglish@icloud.com>
2022-04-07 14:51:54 -0500

    removed left over lines from merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,35 +39,6 @@ class User < ApplicationRecord
 	accepts_nested_attributes_for :subscriptions
 	accepts_nested_attributes_for :owned_groups
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-	def total_cost
-    sum = 0
-    for sub in subscriptions
-      sum += sub.cost_per_month
-    end
-    return (sum).round(2)
-	end
-
-	def total_cost_overall
-    sum = 0
-    for sub in subscriptions
-			sub_date = sub.created_at.to_datetime
-			delta = ((Time.now - sub_date) / 60 / 60 / 24).round
-			num_mo = (delta / 30 + 1)
-      sum +=  sub.cost_per_month * num_mo
-    end
-		return sum
-	end
-=======
-
-	
->>>>>>> initial group statistics page started
-=======
-
-	
->>>>>>> initial group statistics page started
-
   def is_viewer?(group)
 		access = group_access_level(group)
 		return (Membership.permissions[access] >= Membership.permissions[:viewer])
