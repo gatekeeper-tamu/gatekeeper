6571f4d20a9de3af6e60bd1632f4002af5667a7e
Rebecca McFadden <rebecca.mcfadden16@gmail.com>
2022-04-12 17:07:46 -0500

    new account mailer

--- db/schema.rb
+++ db/schema.rb
@@ -42,9 +42,7 @@ ActiveRecord::Schema.define(version: 2022_04_05_165738) do
     t.date "end_date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.uuid "subscription_id", null: false
     t.string "frequency"
-    t.index ["subscription_id"], name: "index_reminders_on_subscription_id"
   end
 
   create_table "shared_subscriptions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -106,7 +104,6 @@ ActiveRecord::Schema.define(version: 2022_04_05_165738) do
   add_foreign_key "groups", "users", on_delete: :cascade
   add_foreign_key "memberships", "groups", on_delete: :cascade
   add_foreign_key "memberships", "users", on_delete: :cascade
-  add_foreign_key "reminders", "subscriptions", on_delete: :cascade
   add_foreign_key "shared_subscriptions", "groups", on_delete: :cascade
   add_foreign_key "shared_subscriptions", "subscriptions", on_delete: :cascade
   add_foreign_key "subscriptions", "users"
