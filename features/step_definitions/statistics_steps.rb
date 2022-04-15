18b508969974cfa8cf187e87d6653d069b8dfaa4
Cora English <52554365+coraenglish@users.noreply.github.com>
2022-04-14 23:08:36 -0500

    [Statistics Feature] - Group statistics (#88) - [Delivers #181144550]
    * initial group statistics page started
    
    * buttons working to change graphs
    
    * button working to switch between everything except for second graph
    
    * legend for second graph shows usernames instead of objects
    
    * fixed buttons for second graph
    
    * separated tabs for cleaner code
    
    * fixed descriptions of graphs and cleaned up indentations
    
    * initial group statistics page started
    
    * buttons working to change graphs
    
    * button working to switch between everything except for second graph
    
    * legend for second graph shows usernames instead of objects
    
    * fixed buttons for second graph
    
    * separated tabs for cleaner code
    
    * fixed descriptions of graphs and cleaned up indentations
    
    * removed unused javascript functions
    
    * db migration
    
    * added functions that were lost in a merge
    
    * removed left over lines from merge
    
    * added more statistics cucumber tests
    
    * added regex to cucumber
    
    * added rspec tests
    
    * removed useless assignments
    
    * [CodeFactor] Apply fixes
    
    * remove unused assignments
    
    Co-authored-by: codefactor-io <support@codefactor.io>
    Co-authored-by: Rebecca McFadden <rebecca.mcfadden16@gmail.com>
    Co-authored-by: Rebecca McFadden <rmcfadden@tamu.edu>

--- features/step_definitions/statistics_steps.rb
+++ features/step_definitions/statistics_steps.rb
@@ -1,9 +1,18 @@
 # include user_steps
+# include subscription_steps
+# include group_steps
 
 ##### GIVEN #####
 Given /I am on the statistics page/ do
 	path = "/statistics"
 	visit path
+end
+
+Given /I am on the (sitewide|group|personal) statistics page/ do |action|
+	path = "/statistics"
+	visit path
+	tab = page.find("a##{action.downcase}-tab.nav-link")
+	tab.click
 end
 
 ##### WHEN #####
@@ -23,4 +32,31 @@ end
 
 Then /^I should see the personal statistics page$/ do
 	page.should have_content("Personal statistics")
+end
+
+Then /^I should see the chart (.*)$/ do |chart|
+	page.should have_content("Loading...")
+end
+
+### personal ###
+Then /^I should see the table showing subscription details$/ do
+	page.should have_content("http://netflix.com")
+end
+
+Then /^I should see my total monthly cost$/ do
+	page.should have_content("Your total monthly cost of serivces on our website is $18.94")
+end
+
+Then /^I should see my total overall cost$/ do
+	page.should have_content("Your total all-time cost of serivces tracked on our website is $18.94")
+end
+
+### group ###
+Then /^I should see my group's total monthly cost$/ do
+	page.should have_content("The total monthly cost for this group is $14.95")
+end
+
+### sitewide ###
+Then /^I should see the total subscription count$/ do
+	page.should have_content("We have a total of 3 accounts stored on gatekeeper.com")
 end
