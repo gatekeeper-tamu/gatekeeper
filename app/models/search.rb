179b81ec4fabee9a6d2f033a44697d960f1c2585
nikhithareddyv <56138975+nikhithareddyv@users.noreply.github.com>
2022-04-30 20:34:29 -0500

    [Cleanup Feature] - Delete duplicate record entries (#118)
    * model deletes duplicate record entries
    
    * codefactor fixes
    
    * codefactor fixes
    
    * Delete .rake_tasks~
    
    Co-authored-by: Rebecca McFadden <rmcfadden@tamu.edu>

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,15 @@
 class Search < ApplicationRecord
+    def self.dedupe
+        # find all models and group them on keys which should be common
+        grouped = all.group_by{|searches| [searches.search_id,searches.title] }
+        grouped.values.each do |duplicates|
+          # the first one we want to keep
+          duplicates.shift
+          # if there are any more left, they are duplicates
+          # so delete all of them
+          duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+        end
+    end
 end
+
+Search.dedupe
