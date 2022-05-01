417926d18f340c2bc0a9c23b79b2d4b9065d03e8
gracenli <gracenecity@gmail.com>
2022-05-01 17:21:03 -0500

    Code Commenting

--- app/models/shared_subscription.rb
+++ app/models/shared_subscription.rb
@@ -1,7 +1,9 @@
 class SharedSubscription < ApplicationRecord
+  #shared subscription must belong to a subscription and group
   belongs_to :subscription
   belongs_to :group
 
+  #the group must be unique
   validates :group_id, :uniqueness => {:scope=>:subscription_id}
 
   enum permission: {
